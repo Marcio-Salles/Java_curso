0a6c71e5ea33277d012a0a7799aea29624c7c5bf
Marcio-Salles <sallesmarcio1978@gmail.com>
2024-01-09 20:03:58 -0300

    Marcio-Salles

--- src/Classe/ProdutoTeste.java
+++ src/Classe/ProdutoTeste.java
@@ -5,13 +5,13 @@ import java.util.Scanner;
 public class ProdutoTeste {
     public static void main(String[] args) {
 
-      Produto p1 = new Produto();
-      p1.nome = "Notebook";
+      Produto p1 = new Produto("Notebook");
+     // p1.nome = "Notebook";
       p1.preco = 4356.89;
       p1.desconto = 0.25;
 
-      var p2 = new Produto();
-        p2.nome = "Caneta Preta";
+      var p2 = new Produto("Caneta Preta");
+     //   p2.nome = "Caneta Preta";
         p2.preco = 12.56;
         p2.desconto = 0.29;
 
