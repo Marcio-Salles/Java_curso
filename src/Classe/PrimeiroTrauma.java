f31e4d8fdff1db35755a2e4feec32dad25f3dcbc
Marcio-Salles <sallesmarcio1978@gmail.com>
2024-01-10 16:53:01 -0300

    Marcio-Salles

---
+++ src/Classe/PrimeiroTrauma.java
@@ -0,0 +1,16 @@
+package Classe;
+
+public class PrimeiroTrauma {
+
+    int a = 3;
+    public static void main(String[] args) {
+
+        PrimeiroTrauma p = new PrimeiroTrauma();
+
+//      Necessário criar a instancia acima para acessar a váriavel fora do main
+
+        System.out.println(p.a);
+
+
+    }
+}
---
+++ src/Classe/ValorVersusReferencia.java
@@ -0,0 +1,28 @@
+package Classe;
+
+
+public class ValorVersusReferencia {
+
+    public static void main(String[] args) {
+
+//        Atribuição por valor
+
+        int a = 2;
+        int b = a;
+
+        System.out.println(b + " e " + a);
+
+//        Atribuição por referência
+
+//        Data p1 = new Data();
+//        p1.dia = 12;
+//        p1.mes = "Julho";
+//        p1.ano = 1984;
+//
+//
+//
+//        System.out.println(p1);
+
+        }
+    }
+
